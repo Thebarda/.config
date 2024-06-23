280f9e3efdbbc86f7cbe059e3c1ec5cae84fcc32
Thebarda <tomdar87@outlook.com>
2024-06-23 17:24:54 +0200

    enable move line plugin

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -60,5 +60,20 @@ return { -- Collection of various small independent plugins/modules
     })
     local notify = require('mini.notify')
     notify.setup()
+    require('mini.move').setup({
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = '<C-Left>',
+        right = '<C-Right>',
+        down = '<C-Down>',
+        up = '<C-Up>',
+
+        -- Move current line in Normal mode
+        line_left = '<C-Left>',
+        line_right = '<C-Right>',
+        line_down = '<C-Down>',
+        line_up = '<C-Up>',
+      },
+    })
   end,
 }
