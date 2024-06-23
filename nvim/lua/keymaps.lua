a529683deedadeab3985135df672f67c6e2e3fe7
Thebarda <tomdar87@outlook.com>
2024-06-23 11:57:13 +0200

    add fancy cmd inpout

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,12 @@
 vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
   desc = "Toggle Spectre"
 })
+
+vim.keymap.set('n', ';', function()
+  vim.ui.input({ prompt = 'Type Vim command' }, function(text)
+    if text == nil then
+      return
+    end
+    vim.cmd(text)
+  end)
+end)
