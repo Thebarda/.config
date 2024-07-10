82e0cfd10eeeca4f7ec39f2716085987dad3a538
Thebarda <tomdar87@outlook.com>
2024-07-10 16:58:21 +0200

    tweak gruvbox

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,10 +1,15 @@
 return {
   'sainnhe/gruvbox-material',
+  lazy = false,
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_enable_bold = true
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_transparent_background = false
+    local g = vim.g
+    g.gruvbox_material_enable_bold = '1'
+    g.gruvbox_material_enable_italic = '1'
+    g.gruvbox_material_transparent_background = '2'
+    g.gruvbox_material_visual = 'blue background'
+    g.gruvbox_material_current_word = 'bold'
+    g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
     vim.cmd.colorscheme 'gruvbox-material'
   end,
 }
