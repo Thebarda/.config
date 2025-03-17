5a0dc4bab81b952ac9254ac8e6efef5c56b9d0b9
Tom Darneix <tomdar87@outlook.com>
2025-03-17 14:54:18 +0100

    fix lualine colors

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,14 +2,53 @@ return {
   'nvim-lualine/lualine.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   opts = function()
-    local defaultColor = { bg = 'NONE', fg = 'NONE', gui = 'NONE' }
-
     return {
       theme = 'tokyonight',
       options = {
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
-        theme = 'auto',
+        theme = {
+          normal = {
+            a = { bg = '#82aaff', fg = '#212121', gui = 'bold' },
+            b = { bg = '#1e2030', fg = '#82aaff' },
+            c = { bg = 'none', gui = 'bold' },
+            x = { bg = 'none', gui = 'bold' },
+            y = { bg = '#1e2030', fg = '#82aaff' },
+            z = { bg = '#82aaff', fg = '#212121', gui = 'bold' },
+          },
+          insert = {
+            a = { bg = '#c3e88d', fg = '#212121', gui = 'bold' },
+            b = { bg = '#1e2030', fg = '#c3e88d' },
+            c = { bg = 'none', gui = 'bold' },
+            x = { bg = 'none', gui = 'bold' },
+            y = { bg = '#1e2030', fg = '#c3e88d' },
+            z = { bg = '#c3e88d', fg = '#212121', gui = 'bold' },
+          },
+          replace = {
+            a = { bg = '#ff757f', fg = '#212121', gui = 'bold' },
+            b = { bg = '#1e2030', fg = '#ff757f' },
+            c = { bg = 'none', gui = 'bold' },
+            x = { bg = 'none', gui = 'bold' },
+            y = { bg = '#1e2030', fg = '#ff757f' },
+            z = { bg = '#ff757f', fg = '#212121', gui = 'bold' },
+          },
+          visual = {
+            a = { bg = '#fca7ea', fg = '#212121', gui = 'bold' },
+            b = { bg = '#1e2030', fg = '#fca7ea' },
+            c = { bg = 'none', gui = 'bold' },
+            x = { bg = 'none', gui = 'bold' },
+            y = { bg = '#1e2030', fg = '#fca7ea' },
+            z = { bg = '#fca7ea', fg = '#212121', gui = 'bold' },
+          },
+          command = {
+            a = { bg = '#ff966c', fg = '#212121', gui = 'bold' },
+            b = { bg = '#1e2030', fg = '#ff966c' },
+            c = { bg = 'none', gui = 'bold' },
+            x = { bg = 'none', gui = 'bold' },
+            y = { bg = '#1e2030', fg = '#ff966c' },
+            z = { bg = '#ff966c', fg = '#212121', gui = 'bold' },
+          },
+        },
         disabled_filetypes = {
           statusline = {
             'dashboard',
@@ -34,13 +73,12 @@ return {
               info = ' ',
               hint = '󰴓 ',
             },
-            color = defaultColor,
           },
-          { 'filetype', icon_only = true, padding = { left = 1, right = 0 }, color = defaultColor },
-          { 'filename', padding = { left = 1, right = 1 }, color = defaultColor },
+          { 'filetype', icon_only = true, padding = { left = 1, right = 0 } },
+          { 'filename', padding = { left = 1, right = 1 } },
         },
         lualine_x = {
-          { 'diff', color = defaultColor },
+          { 'diff' },
         },
         lualine_y = {
           {
