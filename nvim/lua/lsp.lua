8c657cfe05deec367acf991564443a0f1023ee70
Tom Darneix <tomdar87@outlook.com>
2025-04-02 10:48:22 +0200

    fix undefined global and type check

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -7,6 +7,13 @@ vim.lsp.config['luals'] = {
       runtime = {
         version = 'LuaJIT',
       },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
     },
   },
 }
