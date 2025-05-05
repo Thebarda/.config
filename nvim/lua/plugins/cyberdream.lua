f2e457cccecf4ef47d6c4ff383e04bfb63fa9028
Tom Darneix <tomdar87@outlook.com>
2025-05-05 11:28:43 +0200

    use cyberdream

---
+++ nvim/lua/plugins/cyberdream.lua
@@ -0,0 +1,19 @@
+return {
+  'scottmckendry/cyberdream.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('cyberdream').setup {
+      transparent = true,
+      saturation = 0.4,
+      cache = false,
+      highlights = {
+        Normal = { fg = '#dddddd', bg = 'NONE' },
+        NormalFloat = { fg = '#dddddd', bg = 'NONE' },
+        NormalNC = { fg = '#dddddd', bg = 'NONE' },
+      },
+    }
+
+    vim.cmd 'colorscheme cyberdream'
+  end,
+}
