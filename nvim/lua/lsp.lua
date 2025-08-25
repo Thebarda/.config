41a79d2dbd8135ebd05fb146a6f430fc9533c52d
Tom Darneix <tomdar87@outlook.com>
2025-08-25 21:47:16 +0200

    feat: add gdscript lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -85,6 +85,14 @@ vim.lsp.config['markdown'] = {
   root_markers = { '.marksman.toml', '.git' },
 }
 
-vim.lsp.enable { 'luals', 'vtsls', 'biome', 'rust_analyzer', 'cssvars', 'cssls', 'markdown' }
+local port = os.getenv 'GDScript_Port' or '6005'
+local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
+vim.lsp.config['gdscript'] = {
+  cmd = cmd,
+  filetypes = { 'gd', 'gdscript', 'gdscript3' },
+  root_markers = { 'project.godot', '.git' },
+}
+
+vim.lsp.enable { 'luals', 'vtsls', 'biome', 'rust_analyzer', 'cssvars', 'cssls', 'markdown', 'gdscript' }
 
 require('mason').setup()
