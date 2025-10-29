bf595837a9748e5040821574457eb89c59a8c275
Tom Darneix <tomdar87@outlook.com>
2025-10-29 17:42:17 +0100

    feat: apply biome + vtsls code actions on save

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -93,6 +93,69 @@ vim.lsp.config['gdscript'] = {
   root_markers = { 'project.godot', '.git' },
 }
 
-vim.lsp.enable { 'luals', 'vtsls', 'biome', 'rust_analyzer', 'cssvars', 'cssls', 'markdown', 'gdscript' }
+vim.lsp.config['tailwindcss'] = {
+  cmd = { 'tailwindcss-language-server', '--stdio' },
+  filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'vue', 'css' },
+  workspace_required = false,
+}
+
+vim.lsp.enable { 'luals', 'vtsls', 'biome', 'rust_analyzer', 'cssvars', 'cssls', 'markdown', 'gdscript', 'tailwindcss' }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if not client then
+      return
+    end
+
+    -- When the client is Biome, add an automatic event on
+    -- save that runs Biome's "source.fixAll.biome" code action.
+    -- This takes care of things like JSX props sorting and
+    -- removing unused imports.
+    if client.name == 'biome' then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('BiomeFixAll', { clear = true }),
+        callback = function()
+          vim.lsp.buf.format { async = false }
+          vim.lsp.buf.code_action {
+            context = {
+              only = { 'source.fixAll.biome' },
+              diagnostics = {},
+            },
+            apply = true,
+          }
+        end,
+      })
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('BiomeOrganizeImports', { clear = true }),
+        callback = function()
+          vim.lsp.buf.format { async = false }
+          vim.lsp.buf.code_action {
+            context = {
+              only = { 'source.organizeImports.biome' },
+              diagnostics = {},
+            },
+            apply = true,
+          }
+        end,
+      })
+    end
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = { '*.ts', '*.tsx', '*.js', '*.jsx' },
+      group = vim.api.nvim_create_augroup('LSPQuickfix', { clear = true }),
+      callback = function()
+        vim.lsp.buf.code_action {
+          context = {
+            only = { 'quickfix' },
+            diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
+          },
+          apply = true,
+        }
+      end,
+    })
+  end,
+})
 
 require('mason').setup()
