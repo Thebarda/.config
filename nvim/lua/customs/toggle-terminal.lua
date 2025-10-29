1c8b762e966ffeaa2247d60965da41268e3d1cdd
Tom Darneix <tomdar87@outlook.com>
2025-10-29 17:42:39 +0100

    feat: keep the terminal active in a buffer

--- nvim/lua/customs/toggle-terminal.lua
+++ nvim/lua/customs/toggle-terminal.lua
@@ -2,87 +2,19 @@ local window_management = require 'customs.utils.window-manager'
 
 local M = {}
 
+local is_displayed = nil
 local buf
-local win
-local current_float_border_hl
 local api = vim.api
 
-local function change_win_based_on_mode(is_terminal)
-  local current_win_config = api.nvim_win_get_config(win)
-  if is_terminal then
-    current_float_border_hl = api.nvim_get_hl(0, { name = 'FloatBorder' })
-    api.nvim_set_hl(0, 'FloatBorder', { fg = '#9ae876', bg = 'NONE' })
-    api.nvim_win_set_config(win, {
-      relative = current_win_config.relative,
-      col = current_win_config.col,
-      row = current_win_config.row,
-      title = 'Insert enabled.',
-      title_pos = 'left',
-    })
-    return
-  end
-  api.nvim_set_hl(0, 'FloatBorder', current_float_border_hl)
-  api.nvim_win_set_config(win, {
-    relative = current_win_config.relative,
-    col = current_win_config.col,
-    row = current_win_config.row,
-    title = 'Insert disabled. Press "i" or "a" to enable it.',
-    title_pos = 'left',
-  })
-end
-
-local function unload_states()
-  buf = window_management.close_window(buf)
-  api.nvim_set_hl(0, 'FloatBorder', current_float_border_hl)
-  win = nil
-end
-
-local function create_autocmd()
-  api.nvim_create_augroup('CustomsToggleTerminal', { clear = true })
-
-  api.nvim_create_autocmd('WinClosed', {
-    group = 'CustomsToggleTerminal',
-    pattern = tostring(win), -- window ID as string
-    callback = unload_states,
-  })
-
-  api.nvim_create_autocmd('ModeChanged', {
-    group = 'CustomsToggleTerminal',
-    buffer = buf,
-    callback = function()
-      local mode = vim.api.nvim_get_mode().mode
-      change_win_based_on_mode(mode == 't')
-    end,
-  })
-end
-
-local function create_keymap(col)
-  local opts = { noremap = true, silent = true, buffer = buf }
-  vim.keymap.set('n', '<A-Up>', function()
-    local current_height = api.nvim_win_get_height(win) + 1
-    api.nvim_win_set_config(win, {
-      relative = 'editor',
-      height = current_height,
-      row = vim.o.lines - current_height - 1,
-      col = col,
-    })
-  end, opts)
-  vim.keymap.set('n', '<A-Down>', function()
-    local current_height = api.nvim_win_get_height(win) - 1
-    api.nvim_win_set_config(win, {
-      relative = 'editor',
-      height = current_height,
-      row = vim.o.lines - current_height - 1,
-      col = col,
-    })
-  end, opts)
+local function create_keymap()
+  vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 end
 
 function M.open()
-  local width = vim.o.columns
-  local height = math.floor(vim.o.lines / 2)
-  local row = height - 1
-  local col = 0
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
 
   local window_buffer_state = window_management.open_window {
     is_listed = false,
@@ -97,27 +29,47 @@ function M.open()
   }
 
   buf = window_buffer_state.buffer
-  win = window_buffer_state.window
 
   vim.cmd 'terminal'
   vim.cmd 'startinsert'
   vim.api.nvim_buf_set_option(buf, 'buflisted', false)
+  is_displayed = true
 
-  create_keymap(col)
-  create_autocmd()
+  create_keymap()
 
   current_float_border_hl = api.nvim_get_hl(0, { name = 'FloatBorder' })
 end
 
-function M.close()
-  unload_states()
+function M.toggle_buf()
+  if not is_displayed then
+    vim.cmd('hide buffer ' .. buf)
+    is_displayed = false
+  else
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+    local row = math.floor((vim.o.lines - height) / 2)
+    local col = math.floor((vim.o.columns - width) / 2)
+
+    api.nvim_open_win(buf, true, {
+      relative = 'editor',
+      width = width,
+      height = height,
+      row = row,
+      col = col,
+      style = 'minimal',
+      border = 'rounded',
+    })
+    vim.cmd 'startinsert'
+
+    is_displayed = true
+  end
 end
 
 function M.toggle()
   if not buf then
     M.open()
   else
-    M.close()
+    M.toggle_buf()
   end
 end
 
